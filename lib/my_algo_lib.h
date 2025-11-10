75e6e0372dab4f7ae24100d09f329d30de9103f1
k0ccc <sulaev99@mail.ru>
2025-11-10 18:28:49 +0300

    first algo(binary search)

---
+++ lib/my_algo_lib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class Algorithm
+{
+private:
+  /* data */
+public:
+  int Binary_search(std::vector<int> *arr, int numb) const;
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "lib/my_algo_lib.h"
+#include <iostream>
+
+int main()
+{
+  Algorithm algo;
+  std::vector<int> my_sorted_vector = {1, 2, 4, 6, 12, 22, 34, 36, 41, 42, 46};
+  int index = algo.Binary_search(&my_sorted_vector, 34);
+  std::cout << index << std::endl;
+  return 0;
+}
+ 
